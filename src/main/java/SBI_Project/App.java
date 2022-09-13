d00ef3dadc784f17245b833ce717435d0259c52a
Praveen <praveen138kumar@gmail.com>
2022-09-13 22:24:28 +0530

    first commit for sbi_repo

---
+++ src/main/java/SBI_Project/App.java
@@ -0,0 +1,13 @@
+package SBI_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
